1a377068abd9949567459e8b552e11e65817be8f
kriss <kriss.clotilde@gmail.com>
2023-07-16 21:17:41 +0200

    code du header du cv kriss

--- cv_kriss/src/Header.js
+++ cv_kriss/src/Header.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
 import './Header.css'
-
+import { BiLogoJava } from "react-icons/bi";
+import { IoLogoJavascript } from "react-icons/io";
 class Header extends Component {
     render() {
         return (
                 <div className="container">
-
-                    <div style={{margin: '10px'}}>
-                        <img src="https://projet.krissclotilde.com/asset/avatar-gratuit.png" alt="react logo"
-                             style={{width: '200px',}}/>
+                    <div className="container">
+                        <BiLogoJava className="java" size={200} color={"white"} />
+                        <IoLogoJavascript className="java" size={200} color={"white"} />
+                        <div style={{margin: '10px'}}>
+                            <img src="https://projet.krissclotilde.com/asset/avatar-gratuit.png" alt="react logo"
+                                 style={{width: '200px',borderRadius: '50%'}}/>
+                        </div>
                     </div>
                     <div>
                                 <div className="nom">CLOTILDE</div>
                                 <div className="prenom">kriss</div>
-                                 <div className="titre_cv">Developpeur FullStack</div>
+                                 <div className="titrecv">Developpeur FullStack</div>
 
                     </div>
                 </div>
